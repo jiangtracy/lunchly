860d0faf5d06ed47006eec322486cba7437145eb
Tracy Jiang <tracyjiangmx@gmail.com>
2021-01-08 14:36:59 -0800

    completed best customers

--- models/customer.js
+++ models/customer.js
@@ -1,66 +1,66 @@
-"use strict";
+'use strict';
 
 /** Customer for Lunchly */
 
-const db = require("../db");
-const Reservation = require("./reservation");
+const db = require('../db');
+const Reservation = require('./reservation');
 
 /** Customer of the restaurant. */
 
 class Customer {
-  constructor({ id, firstName, lastName, phone, notes }) {
-    this.id = id;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.phone = phone;
-    this.notes = notes;
-  }
+	constructor({ id, firstName, lastName, phone, notes }) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phone = phone;
+		this.notes = notes;
+	}
 
-  /** find all customers. */
+	/** find all customers. */
 
-  static async all() {
-    const results = await db.query(
-      `SELECT id,
+	static async all() {
+		const results = await db.query(
+			`SELECT id,
                   first_name AS "firstName",
                   last_name  AS "lastName",
                   phone,
                   notes
            FROM customers
-           ORDER BY last_name, first_name`,
-    );
-    return results.rows.map(c => new Customer(c));
-  }
+           ORDER BY last_name, first_name`
+		);
+		return results.rows.map((c) => new Customer(c));
+	}
 
-  /** get a customer by ID. */
+	/** get a customer by ID. */
 
-  static async get(id) {
-    const results = await db.query(
-      `SELECT id,
+	static async get(id) {
+		const results = await db.query(
+			`SELECT id,
                   first_name AS "firstName",
                   last_name  AS "lastName",
                   phone,
                   notes
            FROM customers
            WHERE id = $1`,
-      [id],
-    );
+			[ id ]
+		);
 
-    const customer = results.rows[0];
+		const customer = results.rows[0];
 
-    if (customer === undefined) {
-      const err = new Error(`No such customer: ${id}`);
-      err.status = 404;
-      throw err;
-    }
+		if (customer === undefined) {
+			const err = new Error(`No such customer: ${id}`);
+			err.status = 404;
+			throw err;
+		}
 
-    return new Customer(customer);
-  }
+		return new Customer(customer);
+	}
 
-  /* Gets a customer by name */
-  static async getByName(name) {
-    let [firstName, lastName] = name.split(" ");
-    const results = await db.query(
-      `SELECT id,
+	/* Gets a customer by name */
+	static async getByName(name) {
+		let [ firstName, lastName ] = name.split(' ');
+		const results = await db.query(
+			`SELECT id,
               first_name AS "firstName",
               last_name  AS "lastName",
               phone,
@@ -68,60 +68,84 @@ class Customer {
        FROM customers
        WHERE first_name ILIKE $1
        OR last_name ILIKE $2 `,
-      [`%${firstName}%`, `%${lastName}%`],
-    );
-    let customers = [];
-    for (let row of results.rows) {
-      customers.push(new Customer(row));
-    };
-    console.log("Customers: ", customers);
+			[ `%${firstName}%`, `%${lastName}%` ]
+		);
+		let customers = [];
+		for (let row of results.rows) {
+			customers.push(new Customer(row));
+		}
+		console.log('Customers: ', customers);
 
-    if (customers === undefined) {
-      const err = new Error(`No such customer: ${name}`);
-      err.status = 404;
-      throw err;
-    }
+		if (customers === undefined) {
+			const err = new Error(`No such customer: ${name}`);
+			err.status = 404;
+			throw err;
+		}
 
-    return customers;
-  }
-  /* generate a fullname for a customer */
-  fullName() {
-    return `${this.firstName} ${this.lastName}`;
-  }
-  /** get all reservations for this customer. */
+		return customers;
+	}
+	/* generate a fullname for a customer */
 
-  async getReservations() {
-    return await Reservation.getReservationsForCustomer(this.id);
-  }
+	fullName() {
+		return `${this.firstName} ${this.lastName}`;
+	}
+	/** get all reservations for this customer. */
 
-  /** save this customer. */
+	async getReservations() {
+		return await Reservation.getReservationsForCustomer(this.id);
+	}
 
-  async save() {
-    if (this.id === undefined) {
-      const result = await db.query(
-        `INSERT INTO customers (first_name, last_name, phone, notes)
+	/* gereate top 10 customers with highest number of reservations  */
+
+	static async bestCustomers() {
+		//grab top 10 customers' ids
+		const rResults = await db.query(
+			`SELECT customer_id, COUNT(id) 
+        FROM reservations 
+        GROUP BY customer_id
+        ORDER BY COUNT(id) DESC 
+        LIMIT 10`
+		);
+		let bestCustomersIdArray = rResults.rows.map((customer) => Number(customer.customer_id));
+
+		//grab customer name
+		const cResults = await db.query(
+			`SELECT id,
+        first_name AS "firstName",
+        last_name  AS "lastName",
+        phone,
+        notes
+        FROM customers
+        WHERE id IN ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`,
+			[ ...bestCustomersIdArray ]
+		);
+
+		return cResults.rows.map((row) => new Customer(row));
+	}
+
+	/** save this customer. */
+
+	async save() {
+		if (this.id === undefined) {
+			const result = await db.query(
+				`INSERT INTO customers (first_name, last_name, phone, notes)
              VALUES ($1, $2, $3, $4)
              RETURNING id`,
-        [this.firstName, this.lastName, this.phone, this.notes],
-      );
-      this.id = result.rows[0].id;
-    } else {
-      await db.query(
-        `UPDATE customers
+				[ this.firstName, this.lastName, this.phone, this.notes ]
+			);
+			this.id = result.rows[0].id;
+		} else {
+			await db.query(
+				`UPDATE customers
              SET first_name=$1,
                  last_name=$2,
                  phone=$3,
                  notes=$4
-             WHERE id = $5`, [
-        this.firstName,
-        this.lastName,
-        this.phone,
-        this.notes,
-        this.id,
-      ],
-      );
-    }
-  }
+             WHERE id = $5`,
+				[ this.firstName, this.lastName, this.phone, this.notes, this.id ]
+			);
+		}
+	}
 }
 
 module.exports = Customer;
--- routes.js
+++ routes.js
@@ -1,100 +1,101 @@
-"use strict";
+'use strict';
 
 /** Routes for Lunchly */
 
-const express = require("express");
+const express = require('express');
 
-const Customer = require("./models/customer");
-const Reservation = require("./models/reservation");
+const Customer = require('./models/customer');
+const Reservation = require('./models/reservation');
 
 const router = new express.Router();
 
-
 /** Handle searching for customers. */
 
-router.get("/search/", async function (req, res, next) {
-  const name = req.query.search;
-  console.log("Searching for customer name:", name);
-  let customers = await Customer.getByName(name);
+router.get('/search/', async function(req, res, next) {
+	const name = req.query.search;
+	console.log('Searching for customer name:', name);
+	let customers = await Customer.getByName(name);
 
-
-  return res.render("customer_list.html", { customers });
-
+	return res.render('customer_list.html', { customers });
 });
 
 /** Homepage: show list of customers. */
 
-router.get("/", async function (req, res, next) {
-  const customers = await Customer.all();
-  return res.render("customer_list.html", { customers });
+router.get('/', async function(req, res, next) {
+	const customers = await Customer.all();
+	return res.render('customer_list.html', { customers });
+});
+
+/* Show a list of top 10 customers */
+router.get('/best', async function(req, res, next) {
+	const customers = await Customer.bestCustomers();
+	return res.render('customer_best.html', { customers });
 });
 
 /** Form to add a new customer. */
 
-router.get("/add/", async function (req, res, next) {
-  return res.render("customer_new_form.html");
+router.get('/add/', async function(req, res, next) {
+	return res.render('customer_new_form.html');
 });
 
 /** Handle adding a new customer. */
 
-router.post("/add/", async function (req, res, next) {
-  const { firstName, lastName, phone, notes } = req.body;
-  const customer = new Customer({ firstName, lastName, phone, notes });
-  await customer.save();
+router.post('/add/', async function(req, res, next) {
+	const { firstName, lastName, phone, notes } = req.body;
+	const customer = new Customer({ firstName, lastName, phone, notes });
+	await customer.save();
 
-  return res.redirect(`/${customer.id}/`);
+	return res.redirect(`/${customer.id}/`);
 });
 
 /** Show a customer, given their ID. */
 
-router.get("/:id/", async function (req, res, next) {
-  const customer = await Customer.get(req.params.id);
+router.get('/:id/', async function(req, res, next) {
+	const customer = await Customer.get(req.params.id);
 
-  const reservations = await customer.getReservations();
+	const reservations = await customer.getReservations();
 
-  return res.render("customer_detail.html", { customer, reservations });
+	return res.render('customer_detail.html', { customer, reservations });
 });
 
 /** Show form to edit a customer. */
 
-router.get("/:id/edit/", async function (req, res, next) {
-  const customer = await Customer.get(req.params.id);
+router.get('/:id/edit/', async function(req, res, next) {
+	const customer = await Customer.get(req.params.id);
 
-  res.render("customer_edit_form.html", { customer });
+	res.render('customer_edit_form.html', { customer });
 });
 
 /** Handle editing a customer. */
 
-router.post("/:id/edit/", async function (req, res, next) {
-  const customer = await Customer.get(req.params.id);
-  customer.firstName = req.body.firstName;
-  customer.lastName = req.body.lastName;
-  customer.phone = req.body.phone;
-  customer.notes = req.body.notes;
-  await customer.save();
+router.post('/:id/edit/', async function(req, res, next) {
+	const customer = await Customer.get(req.params.id);
+	customer.firstName = req.body.firstName;
+	customer.lastName = req.body.lastName;
+	customer.phone = req.body.phone;
+	customer.notes = req.body.notes;
+	await customer.save();
 
-  return res.redirect(`/${customer.id}/`);
+	return res.redirect(`/${customer.id}/`);
 });
 
 /** Handle adding a new reservation. */
 
-router.post("/:id/add-reservation/", async function (req, res, next) {
-  const customerId = req.params.id;
-  const startAt = new Date(req.body.startAt);
-  const numGuests = req.body.numGuests;
-  const notes = req.body.notes;
+router.post('/:id/add-reservation/', async function(req, res, next) {
+	const customerId = req.params.id;
+	const startAt = new Date(req.body.startAt);
+	const numGuests = req.body.numGuests;
+	const notes = req.body.notes;
 
-  const reservation = new Reservation({
-    customerId,
-    startAt,
-    numGuests,
-    notes,
-  });
-  await reservation.save();
+	const reservation = new Reservation({
+		customerId,
+		startAt,
+		numGuests,
+		notes
+	});
+	await reservation.save();
 
-  return res.redirect(`/${customerId}/`);
+	return res.redirect(`/${customerId}/`);
 });
-
-
 
 module.exports = router;
